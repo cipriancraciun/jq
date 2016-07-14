ad6015e4a4c3b8d18f7c964845c6765ecaeb9ec5
Ciprian Dorin Craciun <ciprian@volution.ro>
2016-07-14 20:56:50 +0300

    [jqe]  Add crypto random function

--- src/_extensions/jqe_crypto.c
+++ src/_extensions/jqe_crypto.c
@@ -9,11 +9,16 @@
 
 #include <openssl/md5.h>
 #include <openssl/sha.h>
+#include <openssl/rand.h>
+
 
 
 
 typedef unsigned char * (* jqe_crypto_hasher) (unsigned char const *, size_t, unsigned char *);
 static jv jqe_crypto_hash (jq_state * jq, jqe_crypto_hasher hasher, size_t hash_size, jv input, jv encode);
+
+static jv jqe_crypto_hexstring (void const * data, size_t size);
+
 
 
 
@@ -95,20 +100,52 @@ jv jqe_crypto_hash (jq_state * jq, jqe_crypto_hasher hasher, size_t hash_size, j
 	size_t string_size = jv_string_length_bytes (jv_copy (input));
 	
 	unsigned char hash_data[hash_size];
-	char hash_text[hash_size * 2 + 1];
-	
 	memset (hash_data, 0, hash_size);
-	memset (hash_text, 0, hash_size * 2 + 1);
 	
 	hasher ((unsigned char const *) string_data, string_size, hash_data);
 	
-	static char hexdigits[] = "0123456789abcdef";
-	for (size_t i = 0; i < hash_size; i += 1) {
-		hash_text[i * 2 + 0] = hexdigits[(hash_data[i] & 0xf0) >> 4];
-		hash_text[i * 2 + 1] = hexdigits[(hash_data[i] & 0x0f) >> 0];
-	}
-	
 	jv_free (input);
 	
-	return (jv_string_sized (hash_text, hash_size * 2));
+	return (jqe_crypto_hexstring (hash_data, hash_size));
+}
+
+
+
+
+jv jqe_crypto_random (jq_state * jq, jv input)
+{
+	if (!jv_is_integer (input)) {
+		jv_free (input);
+		return (jv_invalid_with_msg (jv_string ("`jqe_crypto_random` expected integer input")));
+	}
+	
+	int random_size = (int) jv_number_value (input);
+	
+	if ((random_size < 0) || (random_size > 1024))
+		return (jv_invalid_with_msg (jv_string ("`jqe_crypto_random` invalid input (between 0 and 1024)")));
+	
+	unsigned char random_data[random_size];
+	memset (random_data, 0, random_size);
+	
+	if (!RAND_bytes (random_data, random_size))
+		return (jv_invalid_with_msg (jv_string ("`jqe_crypto_random` failed:  error encountered while generating random data")));
+	
+	return (jqe_crypto_hexstring (random_data, random_size));
+}
+
+
+
+
+static jv jqe_crypto_hexstring (void const * data, size_t size)
+{
+	static char digits[] = "0123456789abcdef";
+	
+	char text[size * 2 + 1];
+	
+	for (size_t i = 0; i < size; i++) {
+		text[i * 2 + 0] = digits[(((unsigned char const *) data)[i] & 0xf0) >> 4];
+		text[i * 2 + 1] = digits[(((unsigned char const *) data)[i] & 0x0f) >> 0];
+	}
+	
+	return (jv_string_sized (text, size * 2));
 }
--- src/_extensions/jqe_crypto.h
+++ src/_extensions/jqe_crypto.h
@@ -25,4 +25,7 @@ extern jv jqe_crypto_sha512 (jq_state * jq, jv input);
 extern jv jqe_crypto_sha512_ll (jq_state * jq, jv input, jv encode);
 
 
+extern jv jqe_crypto_random (jq_state * jq, jv input);
+
+
 #endif
